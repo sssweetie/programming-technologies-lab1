df7c2fccb256b7b62c59a5c8ed0703f1f3ef650f
Alexandr Tokarev <63711180+sssweetie@users.noreply.github.com>
2023-10-12 18:37:11 +0300

    feat: jsonDataReader test

---
+++ test/test_JsonDataReader.py
@@ -0,0 +1,40 @@
+import pytest
+from src.JsonDataReader import JsonDataReader
+from src.Types import DataType
+
+class TestJsonDataReader:
+    @pytest.fixture()
+    def file_and_data_content(self) -> tuple[str, DataType]:
+        text = '''{
+            "Иванов Иван Иванович": {
+                "математика": 67,
+                "литература": 100,
+                "программирование": 91
+            },
+            "Петров Петр Петрович": {
+                "математика": 78,
+                "химия": 87,
+                "социология": 61
+            }
+        }'''
+        data = {
+            "Иванов Иван Иванович": [
+                ("математика", 67), ("литература", 100), ("программирование": 91)
+            ],
+            "Петров Петр Петрович": [
+                ("математика", 78), ("химия", 87), ("социология", 61)
+            ]
+        }
+        return text, data
+
+    @pytest.fixture()
+    def filepath_and_data(self,
+                          file_and_data_content: tuple[str, DataType],
+                          tmpdir) -> tuple[str, DataType]:
+        p = tmpdir.mkdir("datadir").join("my_data.json")
+        p.write_text(json.dumps(file_and_data_content, ensure_ascii=False), encoding='utf-8')
+        return str(p), file_and_data_content
+
+    def test_read(self, filepath_and_data: tuple[str, DataType]) -> None:
+        file_content = JsonDataReader().read(filepath_and_data[0])
+        assert file_content == filepath_and_data[1]
