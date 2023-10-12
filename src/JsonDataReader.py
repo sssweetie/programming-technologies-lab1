c9f8fda8b11d834054c092f52cc0426d4445086a
Alexandr Tokarev <63711180+sssweetie@users.noreply.github.com>
2023-10-12 18:43:48 +0300

    fix: pycodestyle

--- src/JsonDataReader.py
+++ src/JsonDataReader.py
@@ -3,11 +3,12 @@ import json
 from Types import DataType
 from DataReader import DataReader
 
+
 class JsonDataReader(DataReader):
     def read(self, path: str) -> DataType:
         with open(path, encoding='utf-8') as file:
             data = json.load(file)
-        
+
         students: DataType = {}
 
         for name, subjects in data.items():
@@ -15,5 +16,5 @@ class JsonDataReader(DataReader):
             for subject, score in subjects.items():
                 student_scores.append((subject, score))
             students[name] = student_scores
-        
+
         return students
--- test/test_JsonDataReader.py
+++ test/test_JsonDataReader.py
@@ -2,6 +2,7 @@ import pytest
 from src.JsonDataReader import JsonDataReader
 from src.Types import DataType
 
+
 class TestJsonDataReader:
     @pytest.fixture()
     def file_and_data_content(self) -> tuple[str, DataType]:
@@ -19,10 +20,14 @@ class TestJsonDataReader:
         }'''
         data = {
             "Иванов Иван Иванович": [
-                ("математика", 67), ("литература", 100), ("программирование": 91)
+                ("математика", 67),
+                ("литература", 100),
+                ("программирование": 91)
             ],
             "Петров Петр Петрович": [
-                ("математика", 78), ("химия", 87), ("социология", 61)
+                ("математика", 78),
+                ("химия", 87),
+                ("социология", 61)
             ]
         }
         return text, data
@@ -32,7 +37,9 @@ class TestJsonDataReader:
                           file_and_data_content: tuple[str, DataType],
                           tmpdir) -> tuple[str, DataType]:
         p = tmpdir.mkdir("datadir").join("my_data.json")
-        p.write_text(json.dumps(file_and_data_content, ensure_ascii=False), encoding='utf-8')
+        p.write_text(json.dumps(file_and_data_content,
+                     ensure_ascii=False),
+                     encoding='utf-8')
         return str(p), file_and_data_content
 
     def test_read(self, filepath_and_data: tuple[str, DataType]) -> None:
