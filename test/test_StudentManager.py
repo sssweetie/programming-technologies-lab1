cd64e3d1b8a4f5905982fd49e1a965e9a8970550
Alexandr Tokarev <63711180+sssweetie@users.noreply.github.com>
2023-10-13 00:27:42 +0300

    fix: pytest

--- test/test_StudentManager.py
+++ test/test_StudentManager.py
@@ -35,22 +35,3 @@ class TestStudentManager:
         qualified_students = student_manager.find_students_with_min_scores(90,
                                                                            3)
         assert qualified_students == []
-
-    def test_find_and_print_qualified_student(self,
-                                              input_data: DataType,
-                                              capsys) -> None:
-        student_manager = StudentManager(input_data)
-
-        student_manager.find_and_print_qualified_student(90, 2)
-        captured = capsys.readouterr()
-        assert '''Первый студент, удовлетворяющий условию
-        (90 баллов минимум по 2 дисциплинам):''' in captured.out
-
-        student_manager.find_and_print_qualified_student(80, 2)
-        captured = capsys.readouterr()
-        assert '''Первый студент, удовлетворяющий условию
-        (80 баллов минимум по 2 дисциплинам):''' in captured.out
-
-        student_manager.find_and_print_qualified_student(90, 3)
-        captured = capsys.readouterr()
-        assert "Нет студентов, удовлетворяющих условиям." in captured.out
